31b313d673ab0f2836a301a55a1296c46f229590
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2023-08-16 11:02:08 +0200

    locks: updated core. Removed decorators.

--- invenio_cache/errors.py
+++ invenio_cache/errors.py
@@ -6,20 +6,40 @@
 # it under the terms of the MIT License; see LICENSE file for more details.
 """Invenio cache errors."""
 
-from invenio_cache.lock import CachedMutex
-
 
 class LockError(Exception):
     """Base error class for lock-related exceptions."""
 
-    def __init__(self, lock):
+    _description = "Base lock error."
+
+    def __init__(self, lock, message=""):
         """Constructor, a lock can be passed to provide more details on the error."""
         self.lock = lock
 
     def __str__(self):
         """Return str(self)."""
-        return f"Error on lock: {self.lock.lock_id}.  Created at: {self.lock.created}"
+        return f"Error on lock: {self.lock}: {self._description}"
 
 
 class LockedError(LockError):
     """Lock is locked error."""
+
+    _description = "Lock was not released yet."
+
+
+class LockReleaseFailed(LockError):
+    """Error when releasing the lock."""
+
+    _description = "Lock failed to be released."
+
+
+class LockAcquireFailed(LockError):
+    """Error when acquiring the lock."""
+
+    _description = "Lock failed to be acquired."
+
+
+class LockRenewPermissionDenied(LockError):
+    """Error when the renewal failed due to permissions."""
+
+    _description = "Lock failed to be renewed due to lack of permissions."
--- tests/test_locks.py
+++ tests/test_locks.py
@@ -7,12 +7,13 @@
 """Test locks."""
 import pytest
 
+from invenio_cache.errors import LockAcquireFailed
 from invenio_cache.lock import CachedMutex
 
 
 def test_cached_mutex(app):
     """Tests two lock objects trying to acquire/release the same lock."""
-    lock_id = f"test_cache_123"
+    lock_id = "test_cache_123"
 
     # Attempt to retrieve the lock again
     lock = CachedMutex(lock_id)
@@ -20,46 +21,55 @@ def test_cached_mutex(app):
     # Acquire lock for ever (0 means no timeout)
     assert lock.acquire(timeout=0)
 
-    # Second lock can't acquire or release the lock
+    # Second lock can't acquire the lock
     second_lock = CachedMutex(lock_id)
-    assert not second_lock.acquire(timeout=1)
-    assert not second_lock.release()
+    with pytest.raises(LockAcquireFailed):
+        not second_lock.acquire(timeout=1)
 
     # Release lock
     lock.release()
 
     # Second lock can acquire the lock now and release it in the end
     assert second_lock.acquire(timeout=1)
-    assert second_lock.acquired
     assert second_lock.exists()
     assert second_lock.release()
 
 
 def test_cached_mutex_context(app):
     """Tests the CachedMutex context manager."""
-    lock_id = f"test_cache_123"
+    lock_id = "test_cache_123"
 
     with CachedMutex(lock_id) as lock:
         assert lock.acquire(timeout=5)
 
-    # Lock was released
-    second_lock = CachedMutex(lock_id)
-    assert second_lock.acquire(timeout=1)
+    # Lock was released on __exit__ so it can be acquired again
+    with CachedMutex(lock_id) as lock:
+        lock.acquire(timeout=1)
+
+    # Acquire the lock and try to acquire it again : should throw an error
+    with CachedMutex(lock_id) as lock:
+        lock.acquire(timeout=1)
+
+        # Throws if asecond caller tries to acquire the lock
+        with pytest.raises(LockAcquireFailed):
+            with CachedMutex(lock_id) as second_lock:
+                second_lock.acquire(timeout=2)
 
 
 def test_cached_mutex_renew(app):
     """Tests the renewal of a cached lock."""
-    lock_id = f"test_cache_123"
+    lock_id = "test_cache_123"
 
     lock = CachedMutex(lock_id)
 
-    # Acquire lock
+    # First caller acquires lock and renews it
     assert lock.acquire(timeout=1)
-    assert lock.renew(100)
+    assert lock.acquire_or_renew(100)
 
-    # Second lock
+    # Second caller can renew the lock
     second_lock = CachedMutex(lock_id)
-    assert second_lock.renew(100)
+    assert second_lock.acquire_or_renew(100)
+    assert second_lock.exists()
 
 
 def test_cache_failure(app, monkeypatch):
@@ -77,10 +87,7 @@ def test_cache_failure(app, monkeypatch):
     lock = CachedMutex("test_123")
     with pytest.raises(Exception):
         lock.acquire(timeout=60)
-    assert not lock.acquired
 
-    # Mock "acquired" to ``True``so we can test the backend failure.
-    monkeypatch.setattr(lock, "acquired", True)
     with pytest.raises(Exception):
         lock.release()
 
